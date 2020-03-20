dea19e0a66d69522f1577ce4a0e0c7758245f6dd
tommyswang <tommyswang@gmail.com>
2020-03-19 20:53:59 -0400

    Added Strong Attack and Heal handler functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,8 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
-
+const HEAL_VALUE = 20;
+let hasBonusLife = true;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife
@@ -12,13 +14,36 @@ function playSound(s) {
   sound.play();
 }
 
-function attackHandler() {
-  if(currentMonsterHealth >= 0 && currentPlayerHealth >= 0) {
+function attackPlayer() {
+  const initialPlayerHealth = currentPlayerHealth;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentPlayerHealth <= 0 && hasBonusLife) {
+    hasBonusLife = false;
+    removeBonusLife();
+    currentPlayerHealth = initialPlayerHealth;
+    setPlayerHealth(initialPlayerHealth);
+
+    alert('You would be dead but the bonus life saved you!');
+  }
+}
+
+function attackMonster(mode) {
+  let maxDamage;
+  let modeSound;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+    modeSound = 'attack-btn-sound';
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+    modeSound = 'strong-attack-btn-sound';
+  }
+  if(currentMonsterHealth > 0 && currentPlayerHealth > 0) {
     const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    playSound('attack-btn-sound');
+    attackPlayer();
+    playSound(modeSound);
     currentMonsterHealth -= monsterDamage;
-    currentPlayerHealth -= playerDamage;
+
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
     alert('You won!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
@@ -26,9 +51,32 @@ function attackHandler() {
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert('You have a draw!');
   } 
+
 }
 
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
+function healPlayerHandler() {
+  let healValue;
+  if(currentMonsterHealth >= 0 && currentPlayerHealth >= 0) {
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE ) {
+      healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+      healValue = HEAL_VALUE;
+    }
+    attackPlayer();
+    increasePlayerHealth(healValue);
+    currentPlayerHealth += healValue;
+    playSound('heal-btn-sound');
+  }
+}
 
 attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click', playSound);
-healBtn.addEventListener('click', playSound);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
